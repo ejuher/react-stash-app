370b41621ceb1c0e6d3971d4ed16e1053552df00
ejuher <ejuher@gmail.com>
2023-05-19 12:39:18 -0700

    links controller

---
+++ app/controllers/api/v1/links_controller.rb
@@ -0,0 +1,31 @@
+class Api::V1::LinksController < ApplicationController
+  def index
+    if params.dig(:filter, :last_link_id)
+      last_link = Link.find(params[:filter][:last_link_id])
+      render json: Link.where(archived: false).where('created_at > ?', last_link.created_at)
+    else
+      render json: Link.where(archived: false).where('created_at > ?', 1.year.ago)
+    end
+  end
+
+  def create
+    @link = Link.new(link_params)
+    if @link.save
+      render json: @link
+    else
+      render json: { errors: @link.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @link = Link.find(params[:id])
+    @link.update!(link_params)
+    render json: @link
+  end
+
+  private
+
+  def link_params
+    params.permit(:url, :title, :tag, :archived, :faved)
+  end
+end
