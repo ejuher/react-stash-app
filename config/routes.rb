67edb90870d3ff98715135d0417318e55c468bd6
ejuher <ejuher@gmail.com>
2023-06-21 15:51:58 -0700

    remove beers controller and update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  root 'beers#index'
+  root 'home#index'
   
   namespace :api do
     namespace :v1 do
-      get 'beers/index'
-      post 'beers/create'
-      delete 'beers/:id', to: 'beers#destroy'
+      get 'links/index'
+      post 'links/create'
+      patch 'links/update'
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
